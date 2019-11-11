d7c9a417b4f91a91fa793249523c963cf1510d05
C_code <564309357@qq.com>
2019-11-11 16:00:29 +0800

    日期实现

---
+++ 11_8/CDateTime.h
@@ -0,0 +1,49 @@
+#ifndef _CDATE_TIME_H_
+#define _CDATE_TIME_H_
+
+#include<iostream>
+#include<time.h>
+#include<windows.h>
+#include<conio.h>
+using namespace std;
+
+struct Date
+{
+	int year;
+	int month;
+	int day;
+};
+struct Time
+{
+	int hour;
+	int minute;
+	int second;
+};
+
+class CDateTime
+{
+public:
+	CDateTime();
+	CDateTime(int year, int month, int day, int hour, int minute, int second);
+	~CDateTime();
+public:
+	void GetCurDateTime();
+	void DateTimeShow();
+	void DateTimeDShow();
+	void ShowMenology();
+protected:
+	bool IsLeap(int year);
+	int GetMDayByYM(int year, int month);
+public:
+	int GetWeekByYMD(int year, int month, int day);
+	void _ShowMenology(int year, int month);
+public:
+	CDateTime NextDateTime(int n);
+	CDateTime PrevDateTime(int n);
+private:
+	Date m_date;
+	Time m_time;
+};
+
+
+#endif /* _CDATE_TIME_H_ */
