2a36298ec5e59789357dcf50287ced67b2f856d9
C_code <564309357@qq.com>
2019-12-14 00:17:55 +0800

    string OJ

---
+++ 12_12/test.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+class Solution {
+public:
+	string reverseString(string s) {
+		if (s.empty())
+			return s;
+
+		size_t start = 0;
+		size_t end = s.size() - 1;
+
+		while (start < end)
+		{
+			swap(s[start], s[end]);
+			++start;
+			--end;
+		}
+
+		return s;
+	}
+};
